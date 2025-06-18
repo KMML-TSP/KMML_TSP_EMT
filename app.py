d542cd78551b972fc8b014afd2cbfbd21675ee4b
KMML-TSP <kmmltspemt@gmail.com>
2025-06-18 12:52:34 +0530

    DAY 3 Complete
    - Fixed the Main Container Bug.
    - Created a Database and users table.
    - Realized the workflow and Listed it in a .txt file.
    - Connected the SQL database with Streamlit App.
    - Implemented user authentication and logic to login and signup.

--- app.py
+++ app.py
@@ -1,7 +1,54 @@
 import streamlit as st
+import mysql.connector as conn
+
+# SQL - Database
+
+# Database connection
+mydb = conn.connect(host="localhost",user="root",passwd="tspemt",database="tsp_emt")
+# Cursor creation
+mycursor = mydb.cursor()
+
+# User Authentication
+def authenticate_user(userid):
+    sql = "SELECT * FROM users WHERE user_id=%s"
+    val = (userid,)
+    mycursor.execute(sql, val)
+    result = mycursor.fetchone()
+    if result:
+        return True
+
+# Login Authentication
+def authenticate_login(userid, password):
+    sql = "SELECT * FROM users WHERE user_id=%s AND pass=%s"
+    val = (userid, password)
+    mycursor.execute(sql, val)
+    result = mycursor.fetchone()
+    if result:
+        return True
+
+# Signup Authentication
+def authenticate_signup(usertype, userid):
+    sql = "SELECT * FROM users WHERE role=%s AND user_id=%s"
+    val = (usertype, userid)
+    mycursor.execute(sql, val)
+    result = mycursor.fetchone()
+    if result:
+        return True
+    
+# Already Created Check
+def already_created(userid):
+    sql = "SELECT is_active FROM users WHERE user_id=%s"
+    val = (userid,)
+    mycursor.execute(sql, val)
+    result = mycursor.fetchone()
+    if result == (1,):
+        return True
+
+
+# UI Components
 
 # Set up the page
-st.set_page_config(page_title="KMML Login", layout="wide")
+st.set_page_config(page_title="KMML Login", page_icon="Images/KMML.png", layout="wide")
 
 # Inject external CSS
 with open("css/style.css") as f:
@@ -13,35 +60,41 @@ if "select" not in st.session_state:
 
 # Web Pages
 class Pages:
-    def login(self, col1, col2):
-        with col1:
-            st.image("Images/KMML_TSP.jpg")
+    # Login Page
+    def login(self):
+        # with col1:
+        #     st.image("Images/KMML_TSP.jpg")
 
         with col2:
             # Header
             st.header("Login")
 
             # Input fields
-            username = st.text_input("User Name", key="username_input")
+            userid = st.text_input("User ID", key="userid_input")
             password = st.text_input("Password", type="password", key="password_input")
 
             btn, alert = st.columns([3.5, 6.5])
 
-            msg_placeholder = alert.empty()
-
             if btn.button("Login", key="login_button"):
-                if username and password:
-                    msg_placeholder.success("Login successful!")
+                if userid and password:
+                    if userid.isdigit():
+                        userid = int(userid)
+                        if authenticate_user(userid):
+                            if authenticate_login(userid, password):
+                                alert.success("Login successful!")
+                            else:
+                                alert.error("Incorrect Password!")
+                        else:
+                            alert.error("Invalid User ID!")
+                    else:
+                        alert.error("Invalid User ID!")
                 else:
-                    msg_placeholder.error("Please fill the forms.")
-            else:
-                # Always keep some space reserved for the alert
-                msg_placeholder.markdown("<div class='alert-space'></div>", unsafe_allow_html=True)
+                    alert.error("Please fill the forms.")
 
-
-    def signup(self, col1, col2):
-        with col1:
-            st.image("Images/KMML_TSP.jpg")
+    # Signup Page
+    def signup(self):
+        # with col1:
+        #     st.image("Images/KMML_TSP.jpg")
 
         with col2:
             # Header
@@ -49,22 +102,47 @@ class Pages:
 
             # Input fields
             usertype = st.selectbox("Select your Role", ["Admin", "Manager", "Staff", "Intern"], key="usertype_input")
-            username = st.text_input("User Name", key="signup_username_input")
+            userid = st.text_input("User ID", key="userid_input")
 
-            btn, alert = st.columns([4.5, 5.5])
+            btn, alert = st.columns([4, 6])
 
             # Signup button
             if btn.button("Signup", key="signup_button"):
-                if usertype and username:
-                    alert.success("Signup successful!")
+                if usertype and userid:
+                    if userid.isdigit():
+                        userid = int(userid)
+                        if authenticate_user(userid):
+                            if not already_created(userid):
+                                if authenticate_signup(usertype, userid):
+                                    st.session_state.select = "create_account"
+                                    st.rerun()
+                                else:
+                                    alert.error("Invalid User Type!")
+                            else:
+                                alert.error("Account already exists!")
+                        else:
+                            alert.error("Invalid User ID!")
+                    else:
+                        alert.error("Invalid User ID!")
                 else:
                     alert.error("Please fill the forms.")
+
+    # Create Account Page
+    def create_account(self):
+        # with col1:
+            # st.image("Images/KMML_TSP.jpg")
+
+        with col2:
+            # Header
+            st.header("Create Account")
+
+
 
 # NavBar
 blank, title, btn1, btn2 = st.columns([1, 6, 1.5, 1.5], vertical_alignment="bottom", gap="small")
 
 # Title image (logo)
-title.image("Images/KMML.png", use_container_width=True)
+title.image("Images/KMML.png")
 
 # Buttons
 if btn1.button("Login"):
@@ -75,12 +153,17 @@ if btn2.button("Signup"):
 # Main content area
 main_container = st.container(height=400)
 
+pages = Pages()
+
 # Page content
 with main_container:
     col1, col2 = st.columns([5.5, 4.5], gap="medium")
-    pages = Pages()
+
+    col1.image("Images/KMML_TSP.jpg")
 
     if st.session_state.select == "login":
-        pages.login(col1, col2)
+        pages.login()
     elif st.session_state.select == "signup":
-        pages.signup(col1, col2)
+        pages.signup()
+    elif st.session_state.select == "create_account":
+        pages.create_account()
